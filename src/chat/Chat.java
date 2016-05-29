6ac82ef50017beb72be1c70369b01ef2a5ce07a8
Igor Gayvan <igor.gayvan@gmail.com>
2016-05-29 08:37:29 +0300

    Some fixes

--- src/chat/Chat.java
+++ src/chat/Chat.java
@@ -6,6 +6,7 @@
 package chat;
 
 import chat.server.ChatServer;
+import chat.client.ChatClient;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
@@ -16,22 +17,30 @@ import java.util.logging.Logger;
  * @author Igor Gayvan
  */
 public class Chat {
-
+    private static final String SERVER_ADDRESS = "127.0.0.1";
     private static final int SERVER_PORT = 5782;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // создаем сервер
         ChatServer chatServer = null;
         try {
             chatServer = new ChatServer(SERVER_PORT);
             Thread thread = new Thread(chatServer);
 
             thread.start();
+
         } catch (IOException ex) {
             Logger.getLogger(Chat.class.getName()).log(Level.SEVERE, null, ex);
         }
+
+        // создаем клиента для сервера
+        ChatClient chatClient = null;
+        chatClient = new ChatClient(SERVER_ADDRESS,SERVER_PORT);
+        Thread thread = new Thread(chatClient);
+        thread.start();
 
         if (chatServer != null) {
             try (Scanner scanner = new Scanner(System.in)) {
--- src/chat/server/ChatServer.java
+++ src/chat/server/ChatServer.java
@@ -6,6 +6,7 @@
 package chat.server;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -19,47 +20,114 @@ import java.util.logging.Logger;
  */
 public class ChatServer implements Runnable {
 
-    private static final int SERVER_TIMEOUT = 4500;
+    private static final int SERVER_TIMEOUT = 1000;
 
-    private int ServerPort;
-    private ServerSocket ServerSocket;
+    private int port;
+    private ServerSocket serverSocket;
     private boolean listen;
 
-    public ChatServer(int ServerPort) throws IOException {
-        this.ServerPort = ServerPort;
-        this.ServerSocket = new ServerSocket(ServerPort);
-        this.ServerSocket.setSoTimeout(SERVER_TIMEOUT);
+    public ChatServer(int port) throws IOException {
+        this.port = port;
+        this.serverSocket = new ServerSocket(port);
+        this.serverSocket.setSoTimeout(SERVER_TIMEOUT);
     }
 
     @Override
     public void run() {
         listen = true;
+
+        System.out.println("SERVER LISTEN ON " + port);
         while (listen) {
-            try {
-                Socket accept = ServerSocket.accept();
-                accept.setSoTimeout(SERVER_TIMEOUT);
-                accept.setSoLinger(true, 500);
+            try (Socket accept = serverSocket.accept()) {
+                // CONFIG SOCKET
+                accept.setSoLinger(true, SERVER_TIMEOUT/2); // Waiting before close connection
+                accept.setSoTimeout(SERVER_TIMEOUT); // Waiting before throws SocketTimeoutException when read data.
 
+                // Print connected ip address.
+                String clientAddress = accept.getInetAddress().getHostAddress();
+                System.out.println("CONNECTED: " + clientAddress);
+
+                // GET IO
                 DataInputStream dis = new DataInputStream(accept.getInputStream());
+                DataOutputStream dos = new DataOutputStream(accept.getOutputStream());
 
-                String input = dis.readUTF();
-
-                switch (input) {
-                    case "MSG": {
-                        System.out.println(input);
-                    }
-
-                }
-
+                commandHandler(clientAddress, dis, dos);
             } catch (SocketTimeoutException ex) {
                 // NOOP
             } catch (IOException ex) {
                 Logger.getLogger(ChatServer.class.getName()).log(Level.SEVERE, null, ex);
             }
-
         }
     }
 
+    /**
+     * Delegate command.
+     *
+     * @param dis input stream.
+     * @param dos output stream.
+     * @throws IOException
+     */
+    public void commandHandler(String address, DataInputStream dis, DataOutputStream dos) throws IOException {
+        // WORKING WITH SOCKET
+        String command = dis.readUTF();
+        System.out.println("COMMAND: " + command);
+
+        switch (command) {
+            case "PING":
+                pingHandler(dos);
+                break;
+            case "MSG":
+                messageHandler(address, dis, dos);
+                break;
+            default:
+                defaultHandler(dos);
+                break;
+        }
+    }
+
+    public void pingHandler(DataOutputStream dos) throws IOException {
+        dos.writeUTF("PONG");
+        dos.flush();
+    }
+
+    public void messageHandler(String sender, DataInputStream dis, DataOutputStream dos) throws IOException {
+        String recipient = dis.readUTF();
+        String message = dis.readUTF();
+
+        System.out.printf("  Address: %s\n  Message: %s\n", recipient, message);
+        
+        boolean success = false;
+
+        try (Socket client = new Socket(recipient, 5781)) {
+            DataOutputStream client_dos = new DataOutputStream(client.getOutputStream());
+            DataInputStream client_dis = new DataInputStream(client.getInputStream());
+
+            client_dos.writeUTF("MSG");
+            client_dos.flush();
+            client_dos.writeUTF(sender);
+            client_dos.flush();
+            client_dos.writeUTF(message);
+            client_dos.flush();
+            
+            String response = client_dis.readUTF();
+            
+            if ("OK".equals(response)) {
+                success = true;
+            }
+        }
+
+        dos.writeUTF(success ? "OK" : "ERROR:While sending message.");
+        dos.flush();
+    }
+
+    public void defaultHandler(DataOutputStream dos) throws IOException {
+        dos.writeUTF("ERROR:Unknown command!");
+        dos.flush();
+    }
+
+    /**
+     * Set {@link #listen} to false.
+     */
     public void stop() {
         listen = false;
     }
